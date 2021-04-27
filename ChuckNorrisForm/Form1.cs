1f789d2152a9737fdbe4422825b2751a74427f84
hm9811 <hm981198@gmail.com>
2021-04-27 06:37:45 -0700

    Created form and finished #1 problem

---
+++ ChuckNorrisForm/Form1.cs
@@ -0,0 +1,27 @@
+﻿using ChuckNorrisAPI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChuckNorrisForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            Joke jokes = await ChuckNorrisClient.GetRandomJoke();
+            MessageBox.Show(jokes.JokeText);
+        }
+    }
+}
